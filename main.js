efe77a7aadd1ea02e5130565f2703f2e2e2f8b9b
Rymene Benmeddour <rymene7@gmail.com>
2025-10-11 22:20:25 +0100

    Add index.html and configure Electron

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
- 
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:5173');
+  } else {
+    win.loadFile('index.html');
+  }
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => app.quit()); 
